78eeedefa133a61be97fec2851a1460cded699d8
keitayoneda <yoneda-keita863@g.ecc.u-tokyo.ac.jp>
2024-02-07 13:50:16 +0900

    PS1 correct quote

--- scripts/essential.sh
+++ scripts/essential.sh
@@ -24,5 +24,5 @@ GIT_PS1_SHOWUNTRACKEDFILES=true
 GIT_PS1_SHOWSTASHSTATE=true
 GIT_PS1_SHOWUPSTREAM=auto
 
-PS1="\[\033[1;32m\]\u@\h\[\033[1;32m\] \[\033[34m\]\w\[\033[1;31m\]$(__git_ps1 " (%s)")\[\033[31m\]\n>\[\e[m " ' >> ~/.bashrc
+PS1='\[\033[1;32m\]\u@\h\[\033[1;32m\] \[\033[34m\]\w\[\033[1;31m\]$(__git_ps1 " (%s)")\[\033[31m\]\n>\[\e[m ' >> ~/.bashrc
 cp $(pwd)/tmux.conf ${HOME}/.tmux.conf
