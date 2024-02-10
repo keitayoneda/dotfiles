57d407aab1f0eca5d3179e4e66b7794d0df1aa9d
keitayoneda <yoneda-keita@863@g.ecc.u-tokyo.ac.jp>
2024-02-10 15:54:31 +0900

    use here doc

--- scripts/essential.sh
+++ scripts/essential.sh
@@ -13,7 +13,10 @@ sudo apt-get install -y automake gcc cmake
 curl -O https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > ${HOME}/.git-completion.sh
 curl -O  https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > ${HOME}/.git-prompt.sh 
 
-echo 'if [ -f ~/.git-completion.sh ]; then
+cp $(pwd)/tmux.conf ${HOME}/.tmux.conf
+
+cat - << EOS >> ~/.bashrc
+if [ -f ~/.git-completion.sh ]; then
     source ~/.git-completion.sh
 fi
 if [ -f ~/.git-prompt.sh ]; then
@@ -24,5 +27,5 @@ GIT_PS1_SHOWUNTRACKEDFILES=true
 GIT_PS1_SHOWSTASHSTATE=true
 GIT_PS1_SHOWUPSTREAM=auto
 
-PS1='\[\033[1;32m\]\u@\h\[\033[1;32m\] \[\033[34m\]\w\[\033[1;31m\]$(__git_ps1 " (%s)")\[\033[31m\]\n>\[\e[m ' >> ~/.bashrc
-cp $(pwd)/tmux.conf ${HOME}/.tmux.conf
+export PS1='\[\033[1;32m\]\u@\h\[\033[1;32m\] \[\033[34m\]\w\[\033[1;31m\]\$(__git_ps1 " (%s)")\[\033[31m\]\n>\[\e[m '
+EOS
