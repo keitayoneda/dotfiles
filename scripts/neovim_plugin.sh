43c7d2d2054f745c89e88fa4788e32b835472e4c
keitayoneda <yoneda-keita@863@g.ecc.u-tokyo.ac.jp>
2024-02-10 16:04:13 +0900

    fix neovim_plugin.sh

--- scripts/neovim_plugin.sh
+++ scripts/neovim_plugin.sh
@@ -6,9 +6,13 @@ git clone https://github.com/wbthomason/packer.nvim \
 cd ${HOME}
 if [ ! -d ${HOME}/.config ]; then
   mkdir ${HOME}/.config 
+fi
+
 ln -s ${HOME}/dotfiles/nvim ${HOME}/.config/nvim
 
 # nerd-font
+sudo apt-get install fontconfig
+
 curl -fLo "FiraCodeNerdFont-Regular.ttf" \
 https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/FiraCode/Regular/FiraCodeNerdFont-Regular.ttf
 
