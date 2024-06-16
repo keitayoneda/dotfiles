9dcbd59c6fe46c903e5e0a85f3167360caa2b3aa
keita yoneda <yoneda-keita863@g.ecc.u-tokyo.ac.jp>
2024-06-16 11:55:30 +0900

    install clangd-12 & pyright

--- scripts/install_neovim_plugin.sh
+++ scripts/install_neovim_plugin.sh
@@ -8,3 +8,10 @@ fi
 cd ${HOME}/.config
 ln -s ${HOME}/dotfiles/nvim nvim
 cd ${HOME}/dotfiles/scripts 
+
+# install clangd
+sudo apt install clangd-12 -y
+sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-12 100
+
+sudo snap install pyright --classic
+
