756309d61cc13529764ee6d738284173a020e4a1
keitayoneda <yoneda@jsk.imi.i.u-tokyo.ac.jp>
2024-10-20 22:47:01 +0900

    add clangd install script

--- scripts/install_essentials.sh
+++ scripts/install_essentials.sh
@@ -24,3 +24,7 @@ sudo apt-get install -y unzip
 # mozc
 sudo apt-get install -y ibus-mozc
 sudo apt-get install -y mozc-utils-gui
+
+# clangd
+sudo apt-get install -y clangd-12
+sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-12 100
