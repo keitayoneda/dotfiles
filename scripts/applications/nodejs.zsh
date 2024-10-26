831360bcc10338deeb002a5adaf35587cd904203
keitayoneda <yoneda-keita@g.ecc.u-tokyo.ac.jp>
2024-10-26 21:05:58 +0900

    add comment to install_nodejs script

--- scripts/applications/nodejs.zsh
+++ scripts/applications/nodejs.zsh
@@ -1,4 +1,6 @@
 #!/usr/bin/env zsh
+
+# install nodejs, npm via apt -> install n via npm -> install stable nodejs via n -> remove nodejs, npm
 sudo apt update
 sudo apt install nodejs npm -y
 sudo npm -g install n
